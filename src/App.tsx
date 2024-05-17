e47c6c299623facc0f6af90e7abfdb5493b220a7
Ramón Alberto Gómez Urquídez <gomez.ramon2004@gmail.com>
2024-05-17 11:28:17 -0700

    Add: TODO

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter } from "react-router-dom"
 import { DesktopNavbar, MobileNavbar, Header, AboutMe, Skills, Projects, ContactMe } from './components/';
 
+// TODO: Consider Refactoring animations to only handle it when mobile/desktop viewport changes (maybe applying a mediator pattern design)
+
 const App = () => {
   return (
     <BrowserRouter>
