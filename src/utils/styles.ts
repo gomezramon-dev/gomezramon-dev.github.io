ba6ee1df2f04ff70649b15d7d39d135328f1de81
Ramón Alberto Gómez Urquídez <gomez.ramon2004@gmail.com>
2024-05-19 15:03:06 -0700

    Delete: Unused Files

--- src/utils/styles.ts
+++
@@ -1,5 +0,0 @@
-export const navbarResponsiveStyle = {
-  animationOnDesktop: "animate-slideUp lg:animate-slideDown",
-  animationOnMobile: "animate-slideDown lg:animate-slideUp",
-  animationOnMobileWindow: `animate-slideDown lg:animate-slideUpWindow`,
-};
