d51eb160f4b23e10e2fe682a85eec8494576dfc2
Ramón Alberto Gómez Urquídez <gomez.ramon2004@gmail.com>
2024-05-29 18:23:27 -0700

    Feature: Added contactme section

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -1,7 +1,19 @@
 const ContactMe = () => {
   return (
-    <div className="flex h-[1000px] items-center justify-center">
-      <h1 className="text-9xl">contact-me</h1>
+    <div className="flex flex-col h-[920px] items-center justify-center gap-16">
+      <h1 className="text-8xl">contact-me</h1>
+      <div className="flex gap-16">
+        <button className="w-[260px] h-[52px]">
+          <a className="flex gap-4" target="_blank" href="https://github.com/gomezramon2004">
+            Github Account
+          </a>
+        </button>
+        <button className="w-[300px] h-[52px]">
+          <a className="flex gap-4" target="_blank" href="https://www.linkedin.com/in/gomezramon2004/">
+            LinkedIn Profile
+          </a>
+        </button>
+      </div>
     </div>
   );
 };
